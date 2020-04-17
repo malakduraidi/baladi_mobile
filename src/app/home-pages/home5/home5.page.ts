a8f708b723534f8fe6dbf8a4f13222d127d5fe2f
System Administrator <root@malaks-MacBook-Pro.local>
2020-04-17 14:00:08 +0300

    supply home

--- src/app/home-pages/home5/home5.page.ts
+++ src/app/home-pages/home5/home5.page.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ConfigService } from 'src/providers/config/config.service';
 import { SharedDataService } from 'src/providers/shared-data/shared-data.service';
+import { Observable } from 'rxjs';
+import { IProductTemplate, ProductTemplateOdooFields } from 'src/app/product-template/models/product-template';
+import { Store } from '@ngrx/store';
+import * as fromProductTemplateStore from "src/app/product-template/store/state";
+import * as fromProductTemplateSelectors from "src/app/product-template/store/selectors";
+import * as fromProductTemplateActions from "src/app/product-template/store/actions";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home5',
@@ -10,11 +17,17 @@ import { SharedDataService } from 'src/providers/shared-data/shared-data.service
 })
 export class Home5Page implements OnInit {
 
-  constructor(
+  customers_basket_quantity = [0,0,0,0,0,0,0,0,0,0,0,0,]
+
+
+  public productTemplates$: Observable < IProductTemplate[] >;
+
+  constructor(private sanitizer:DomSanitizer,
     public nav: NavController,
     public config: ConfigService,
     // public events: Events,
     public shared: SharedDataService,
+    private productTemplateStore: Store <fromProductTemplateStore.ProductTemplateState >,
   ) {
 
   }
@@ -32,6 +45,19 @@ export class Home5Page implements OnInit {
       this.nav.navigateForward(this.config.currentRoute + "/products/" + parent.id + "/" + parent.name + "/newest");
   }
   ngOnInit() {
+    this.productTemplates$ = this.productTemplateStore.select<any>(fromProductTemplateSelectors.selectAllData);
+    this.productTemplateStore.dispatch(new fromProductTemplateActions.LoadHTTP({ domain: [], limit: 10, offset: 0, fields:ProductTemplateOdooFields }));
+  }
+
+  qunatPlus = function (q) {
+    this.customers_basket_quantity[q.id]++;
+  }
+  //function decreasing the quantity
+  qunatMinus = function (q) {
+    if (this.customers_basket_quantity[q.id] == 1) {
+      return 0;
+    }
+    this.customers_basket_quantity[q.id]--;
   }
 
 }
